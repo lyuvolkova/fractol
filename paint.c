67fcf90e71add676ae1e429955a88554f439d1b1
lyuvolkova <80259384+lyuvolkova@users.noreply.github.com>
2022-01-27 07:46:37 +0300

    Add files via upload

--- paint.c
+++ paint.c
@@ -49,6 +49,68 @@ int get_color(int i, int iteration_count)
     return (red << 16 | green << 8 | blue);
 }
 
+int iterate_julia(t_complex *c)
+{
+    int			i;
+	t_complex	z, k;
+
+	i = 0;
+    k = init_complex(-0.4, 0.6);
+	z = init_complex(c->re, c->im);
+	while (pow(z.re, 2.0) + pow(z.im, 2.0) <= 4 && i < MAX_ITERATION)
+	{
+		z = init_complex(
+			pow(z.re, 2.0) - pow(z.im, 2.0) + k.re,
+			2.0 * z.re * z.im + k.im);
+            i++;
+    }
+    return(i);
+
+}
+
+void julia(t_data *img, int iteration_count)
+{
+    int i;
+    int y, x;
+    t_complex c;
+    t_complex min;
+    t_complex max;
+    double factor;
+    int size, offset_x, offset_y;
+
+    offset_y = (HEIGHT - WIDTH) / 2;
+    offset_x = 0;
+    size = WIDTH;
+    if (size > HEIGHT) {
+        size = HEIGHT;
+        offset_x = (WIDTH - HEIGHT) / 2;
+        offset_y = 0;
+    }
+
+    factor = 4.0 / (double)size;
+    
+    min.re = -2.0;
+    max.re = 2.0;
+    min.im = -2.0;
+    max.im = 2.0;
+
+    y=0;
+    while(y < size)
+    {
+        c.im = max.im - y * factor;
+        x = 0;
+        while (x < size)
+        {
+            c.re = min.re + x * factor;
+            i = iterate_julia(&c);
+            my_mlx_pixel_put(img, x + offset_x, y + offset_y, get_color(i, iteration_count));
+            x++;
+        }
+        y++; 
+    }
+}
+
+
 void mandelbrot(t_data *img, int iteration_count)
 {
     int i;
@@ -115,7 +177,8 @@ void paint_fractol(int type, t_data *img)
     if(FRACTOL_JULIA == type)
     {
         //my_mlx_pixel_put(img, 5, 5, 0x00FF0000);
-        fill_rectangle(img,5,5,40,100, 0x00FF0000);
+        //fill_rectangle(img,5,5,40,100, 0x00FF0000);
+        julia(img, 50);
 
     }
     else if(FRACTOL_MALD == type)
